8b1190cfda40dd10e5f99ebc8d1558754e843838
LHS <gustjs1234596@gmail.com>
2024-11-13 23:50:32 +0900

    점프

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -10,11 +10,25 @@ public class Player : MonoBehaviour
     SpriteRenderer spriteRenderer;
     Animator anim;
 
+    private bool isGround;
+    private float jumpScan = 1.5f;
+    private bool isLongJump;
+
     [SerializeField] private Vector2 inputVec;
+
+    [Header("[기능 활성화/비활성화]")]
+    [SerializeField] private bool onMove;
+    [SerializeField] private bool onJump;
+
     [Header("플레이어 이동속도")]
     [SerializeField] private float speed;
     [Header("플레이어 점프력")]
     [SerializeField] private float jumpPower;
+
+    [Header("점프력")]
+    [SerializeField] private float lowJump = 5;
+    [SerializeField] private float highJump = 2;
+    
 
     private void Awake()
     {
@@ -27,7 +41,6 @@ public class Player : MonoBehaviour
     {
         Vector2 nextVec = new Vector2(inputVec.x * speed * Time.fixedDeltaTime, rigid.velocity.y);
         rigid.velocity = new Vector2(inputVec.x * speed, rigid.velocity.y);
-        //rigid.MovePosition(new Vector2(rigid.position.x + nextVec.x, rigid.velocity.y));
 
         if (inputVec.x > 0 || inputVec.x < 0)
         {
@@ -36,6 +49,38 @@ public class Player : MonoBehaviour
         else
         {
             anim.SetBool("isWalk", false);
+        }
+
+        RaycastHit2D jumpRay = Physics2D.Raycast(transform.position, Vector2.down, jumpScan, LayerMask.GetMask("Platform"));
+        Debug.DrawRay(transform.position, Vector2.down * jumpScan, Color.red);
+
+        if (jumpRay.collider != null && jumpRay.collider.gameObject.layer == LayerMask.NameToLayer("Platform"))
+        {
+            if (rigid.velocity.y == 0)
+            {
+                isGround = true;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            isLongJump = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isLongJump = false;
+        }
+
+        if (isLongJump && rigid.velocity.y > 0)
+        {
+            rigid.gravityScale = highJump;
+        }
+        else
+        {
+            rigid.gravityScale = lowJump;
         }
     }
 
@@ -58,7 +103,10 @@ public class Player : MonoBehaviour
 
     private void OnJump(InputValue value)
     {
-        rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-        Debug.Log("점프");
+        if (isGround)
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            isGround = false; 
+        }
     }
 }
