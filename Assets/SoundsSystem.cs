68c77ff0f4d9d12a25e389532f4a824fbf4cb34f
LHS <gustjs1234596@gmail.com>
2024-12-02 01:10:02 +0900

    사운드 제작중

---
+++ Assets/SoundsSystem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundsSystem : MonoBehaviour
+{
+    [SerializeField] private GameObject BGMAdress;
+    [SerializeField] private GameObject SFXAdress;
+
+    public void SFXSoundsMake(AudioClip sound)
+    {
+
+    }
+
+    public void BGMSoundMake(AudioClip sound)
+    {
+
+    }
+}
