c237e29149549112db61f6baf4c6bdac265a1ea3
LHS <gustjs1234596@gmail.com>
2024-12-04 16:06:14 +0900

    dd

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScreenSystem.instance.ScreenPlay(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
